49147a14ffee19c256298d089a23726b915d785f
Denys Lontkovskyi <dlont88@gmail.com>
2025-02-26 15:14:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Requirements
+ * python3.9
+ * poetry
+
+# Installation
+ ```bash
+ curl -sSL https://install.python-poetry.org | python3 -
+
+ ```
+# Usage
+
+ ```bash
+ poetry run python bc.py --help
+ poetry run python bc.py "Hello, Inna" my_barcode
+ ```
---
+++ bc.py
@@ -0,0 +1,22 @@
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def barcode(text: str, out: str):
+    print(f"Text: {text}")
+
+    from io import BytesIO
+
+    from barcode import Code128
+    from barcode.writer import SVGWriter
+
+    # Or to an actual file:
+    with open(f"{out}.svg", "wb") as f:
+        Code128(text, writer=SVGWriter()).write(f)
+
+
+
+if __name__ == "__main__":
+    app()
