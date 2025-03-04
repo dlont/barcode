8483d94d32d20d704ca1f43c22227306b42205db
Denys Lontkovskyi <dlont88@gmail.com>
2025-03-04 16:41:40 +0100

    Add png option

--- bc.py
+++ bc.py
@@ -6,18 +6,19 @@ app = typer.Typer()
 
 
 @app.command()
-def barcode(text: str, out: str, print_text: Optional[bool] = True):
+def barcode(text: str, out: str, print_text: Optional[bool] = True, ext: Optional[str] = 'svg'):
     print(f"Text: {text}")
 
     import barcode
     barcode.base.Barcode.default_writer_options['write_text'] = print_text
 
     from barcode import Code128
-    from barcode.writer import SVGWriter
+    from barcode.writer import SVGWriter, ImageWriter
 
-    # Or to an actual file:
-    with open(f"{out}.svg", "wb") as f:
-        Code128(text, writer=SVGWriter()).write(f)
+    writer = SVGWriter()
+    if ext == 'png': writer = ImageWriter()
+    with open(f"{out}.{ext}", "wb") as f:
+        Code128(text, writer=writer).write(f)
 
 
 
