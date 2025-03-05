21d4e23a61e6f16f6946b436e7c965ad2e9d1936
Denys Lontkovskyi <dlont88@gmail.com>
2025-03-05 15:42:33 +0100

    make height one of the command line parameters

--- bc.py
+++ bc.py
@@ -6,11 +6,27 @@ app = typer.Typer()
 
 
 @app.command()
-def barcode(text: str, out: str, print_text: Optional[bool] = True, ext: Optional[str] = 'svg'):
+def barcode(text: str,
+            out: str,
+            print_text: Optional[bool] = True,
+            ext: Optional[str] = 'svg',
+            height: Optional[float] = 5.0):
     print(f"Text: {text}")
+
+    # Set writer options to customize barcode appearance
+    # writer_options = {
+    #     "module_height": height,  # Height of the barcode modules (the bars)
+    #     "module_width": 0.2,      # Width of each barcode module (optional)
+    #     "quiet_zone": 6.5,        # Margin around the barcode (optional)
+    #     "font_size": 10,          # Font size for the human-readable text (optional)
+    #     "text_distance": 5.0,     # Distance between the barcode and text (optional)
+    #     "background": "white",    # Background color (optional)
+    #     "foreground": "black",    # Barcode color (optional)
+    # }
 
     import barcode
     barcode.base.Barcode.default_writer_options['write_text'] = print_text
+    barcode.base.Barcode.default_writer_options['module_height'] = height
 
     from barcode import Code128
     from barcode.writer import SVGWriter, ImageWriter
